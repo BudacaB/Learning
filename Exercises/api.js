7df4b8fa422c1d9ccaed7470e3bdfaad08c29055
BudacaB <budaca.b@gmail.com>
2018-09-24 20:34:48 +0300

    Eleventh upload, homework update

---
+++ Exercises/api.js
@@ -0,0 +1,54 @@
+//https://data.police.uk/api/forces
+
+//https://data.police.uk/api/forces/leicestershire
+
+const rp = require("request-promise");
+
+
+class PoliceApi {
+    getName(name){
+        return "Hello " + name;
+    }
+    getListOfForces(link){
+        rp.get(link).then(
+            function parcurgere(raspuns){
+                let raspunsFinal = JSON.parse(raspuns);
+                console.log(raspuns);
+                for (let i = 0; i < raspunsFinal.length; i++){
+                    //console.log(raspuns.name)
+                    let acestObiect = raspunsFinal[i];
+                    console.log(acestObiect.name)
+                }
+            }
+        )
+
+    }
+
+
+
+    getSpecificForce(link){
+        rp.get(link).then(
+            function parcurgere(raspuns){
+                let raspunsFinal = JSON.parse(raspuns);
+                let arrayulMeu = raspunsFinal['engagement_methods'];
+                for (let i = 0; i < arrayulMeu.length; i++){
+                    let acestObiect = arrayulMeu[i];
+                    console.log(acestObiect.url)
+                }
+            }
+        )
+
+    }
+
+
+}
+
+//Api.makeWebCall('https://data.police.uk/api/forces')
+
+module.exports = PoliceApi
+
+
+
+
+
+

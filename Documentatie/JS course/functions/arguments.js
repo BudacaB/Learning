c074a4c4735d31d3967d577086a69905e056ad61
BudacaB <budaca.b@gmail.com>
2018-11-08 23:51:49 +0200

    17th upload

--- Documentatie/JS course/functions/arguments.js
+++ Documentatie/JS course/functions/arguments.js
@@ -8,7 +8,8 @@ console.log(result)
 
 // Default arguments
 let getScoreText = function (name = 'Anonymous', score = 0) {
-    return 'Name: ' + name + ' - Score: ' + score
+    // return 'Name: ' + name + ' - Score: ' + score
+    return `Name: ${name} - Score: ${score}`
 }
 
 let scoreText = getScoreText(undefined, 99)
@@ -16,8 +17,9 @@ console.log(scoreText)
 
 // Challenge area
 // total, tipPercent .2 or .1
+
 let getTip = function (total, tipPercent = .2) {
-    return 'The tip for a bill of ' + `${total}` + ' is: ' + total * tipPercent
+    return `The tip for a bill of ${total} is: ` + total * tipPercent
 }
 
 let tip = getTip(100)
@@ -25,3 +27,5 @@ console.log(tip)
 
 let tip2 = getTip(50, .1)
 console.log(tip2)
+
+

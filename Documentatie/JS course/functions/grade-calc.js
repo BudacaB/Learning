91b88adf936a219400194960aed5e2e17b401e3a
BudacaB <budaca.b@gmail.com>
2018-11-10 07:27:34 +0200

    19th upload

--- Documentatie/JS course/functions/grade-calc.js
+++ Documentatie/JS course/functions/grade-calc.js
@@ -4,21 +4,25 @@
 
 let gradeCalc = function(studentScore, possibleScore) {
     let percent = studentScore / possibleScore * 100
-    if (90 <= percent && percent <= 100) {
-        return `You got an A (${percent}%)`
+    let letterGrade = ''
+
+    if (90 <= percent) {
+        letterGrade = 'A'
     }
-    else if (80 <= percent && percent <= 89) {
-        return `You got a B (${percent}%)`
+    else if (80 <= percent) {
+        letterGrade = 'B'
     }
-    else if (70 <= percent && percent <= 79) {
-        return `You got a C (${percent}%)`
+    else if (70 <= percent) {
+        letterGrade = 'C'
     }
-    else if (60 <= percent && percent <= 69) {
-        return `You got a D (${percent}%)`
+    else if (60 <= percent) {
+        letterGrade = 'D'
     }
-    else if (0 <= percent && percent <= 59) {
-        return `You got a F (${percent}%)`
+    else {
+        letterGrade = 'F'
     }
+
+    return `You got a ${letterGrade} (${percent}%)`
 }
 
 let result = gradeCalc(87, 100)

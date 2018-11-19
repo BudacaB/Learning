cc27ceb0d9175019b4a610bdf97cb713ac7062cd
BudacaB <budaca.b@gmail.com>
2018-11-19 21:55:38 +0200

    24th upload

---
+++ Documentatie/C#/FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace FirstConsoleProject
+{
+    class MainClass
+    {
+        static void Main(string[] args)     // this is a method called "Main". It is called when the program starts.
+        {
+            int numero1;
+            int numero2;
+
+            Console.Write ("Type a dividend: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Type a divisor: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The divison between " + numero1 + " and " + numero2 + " is " + numero1 / numero2 );
+            Console.ReadKey();
+        }
+    }
+}

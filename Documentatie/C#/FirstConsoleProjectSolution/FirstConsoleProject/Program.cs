a453cc052b2a08603ada01c0ca34f11b6ae8c750
BudacaB <budaca.b@gmail.com>
2018-11-26 01:40:53 +0200

    28th commit

--- Documentatie/C#/FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ Documentatie/C#/FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -27,9 +27,9 @@ namespace FirstConsoleProject
             {
                 for (int y = 0; y < height; y++)
                 {
-                    Console.WriteLine();
+                    Console.Write(grid[x, y] + " ");
                 }
-                Console.Write(grid[x, y] + " ");
+                Console.WriteLine();
             }
         }
 

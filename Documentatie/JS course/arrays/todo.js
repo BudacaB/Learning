a51ca1689a5b1995e53b4947b25a433f473077e3
BudacaB <budaca.b@gmail.com>
2018-11-28 01:10:50 +0200

    30th upload

---
+++ Documentatie/JS course/arrays/todo.js
@@ -0,0 +1,8 @@
+// Create an array with 5 different todos
+// You have x todos
+// Print the first and second to last items -> Todo: talk the dog
+
+const todos = ['workout', 'brush teeth', 'learn German', 'learn to code', 'do dishes']
+console.log(`You have ${todos.length} todos`)
+console.log('First todo: ' + todos[0])
+console.log('Second to last todo: ' + todos[todos.length - 2])

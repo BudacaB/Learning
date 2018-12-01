19c4122bb5ef6e18962a53d835eecc0e50700fc7
BudacaB <budaca.b@gmail.com>
2018-12-01 02:32:08 +0200

    32nd upload

--- Documentatie/C#/FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
+++ Documentatie/C#/FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -3,44 +3,37 @@ using System.Collections.Generic;
 
 namespace FirstConsoleProject
 {
+    enum Breed { Bulldog, Boxer, Chihuahua, Briard };
 
+    // Base class
     class Animal
     {
-        // CLASS VARIABLES
-
-        public static int Count = 0;
-
         public string name;
         public int age;
         public float happiness;
+    }
 
-        // CLASS CONSTRUCTORS
+    // Derived class
+    class Dog: Animal
+    {
+        public Breed breed;
 
-        public Animal ()
-        {
-            name = "Bruno";
-            age = 6;
-            happiness = 0.5f;
-
-            Count++;
-        }
-
-        public Animal (string _name, int _age, float _happiness)
+        public Dog (string _name, int _age, float _happiness, Breed _breed)
         {
             name = _name;
             age = _age;
             happiness = _happiness;
 
-            Count++;
+            breed = _breed;
         }
-
-        // CLASS METHODS
 
         public void Print ()
         {
             Console.WriteLine("Name: " + name);
             Console.WriteLine("Age: " + age);
             Console.WriteLine("Happiness: " + happiness);
+
+            Console.WriteLine("Breed: " + breed);
         }
     }
 
@@ -48,18 +41,8 @@ namespace FirstConsoleProject
     {
         static void Main(string[] args)     // this is a method called "Main". It is called when the program starts
         {
-
-            Animal dog = new Animal();
-            dog.Print();
-
-            Console.WriteLine();
-
-            Animal cat = new Animal("Tom", 10, 0.8f);
-            cat.Print();
-
-            Console.WriteLine();
-            Console.WriteLine("Num of animals: " + Animal.Count);
-
+            Dog hulk = new Dog("Hulk", 6, 0.7f, Breed.Boxer);
+            hulk.Print();
 
             Console.ReadKey();
         }
--- Documentatie/JS course/arrays/todo.js
+++ Documentatie/JS course/arrays/todo.js
@@ -1,13 +1,14 @@
 const todos = ['workout', 'brush teeth', 'learn German', 'learn to code', 'do dishes']
 
-// Delete the 3rd item
 todos.splice(2, 1)
-// Add a new item onto the end
 todos.push('get food')
-// Remove the first item from the list
 todos.shift()
 
 console.log(`You have ${todos.length} todos`)
-console.log(todos)
+
+todos.forEach(function (todo, index) {
+    console.log(`${index + 1}. ${todo}`)
+})
+
 
 

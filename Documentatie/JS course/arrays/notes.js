cd1687e63a6a438a47f0fee049de863cc680e310
BudacaB <budaca.b@gmail.com>
2018-12-12 02:11:11 +0200

    37th upload, Angular Splitting Apps into Components, Splitting Apps into Components, Binding to Custom Properties

--- Documentatie/JS course/arrays/notes.js
+++ Documentatie/JS course/arrays/notes.js
@@ -1,4 +1,13 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habits to work on',
+    body: 'Exercise. Eating a bit better'
+}, {
+    title: 'Office modifications',
+    body: 'Get a new seat'
+}]
 
 /* console.log(notes.pop())
 notes.push('My new note')
@@ -8,13 +17,18 @@ notes.unshift('My first note') */
 
 // notes.splice(1, 1, 'This is the new 2nd item')
 
-notes[2] = 'This is now the new note 3'
+// notes[2] = 'This is now the new note 3'
 
-notes.forEach(function (item, index) {
-    console.log(index)
-    console.log(item)
-})
+// notes.forEach(function (item, index) {
+//     console.log(index)
+//     console.log(item)
+// })
 
 console.log(notes.length)
 console.log(notes)
 
+const index = notes.findIndex(function (note, index) {
+    console.log(note)
+    return note.title === 'Habits to work on'
+})
+console.log(index)

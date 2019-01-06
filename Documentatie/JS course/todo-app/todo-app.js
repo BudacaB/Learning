eb92cc0b8c5948ac940744b6b730b4dd0bce204a
BudacaB <budaca.b@gmail.com>
2019-01-06 02:54:50 +0200

    59th upload, JS DOM, React jsx

--- Documentatie/JS course/todo-app/todo-app.js
+++ Documentatie/JS course/todo-app/todo-app.js
@@ -1,7 +1,33 @@
-const ps = document.querySelectorAll('p')
+const todos = [{
+    title: 'Workout',
+    completed: true
+    }, {
+    title: 'Brush teeth',
+    completed: true
+    }, {
+    title: 'Learn German',
+    completed: true
+    }, {
+    title: 'Learn to code',
+    completed: false
+    }, {
+    title: 'Do dishes',
+    completed: false
+}]
 
-ps.forEach(function(p) {
-    if (p.textContent.includes('the')) {
-        p.remove();
-    }
+// You have 2 todos left (p element)
+// Add a p for each todo above (use text value)
+
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed
+})
+
+const todosLeft = document.createElement('h2');
+todosLeft.textContent = `You have ${incompleteTodos.length} todos left`;
+document.querySelector('body').appendChild(todosLeft)
+
+todos.forEach(function (todo) {
+    const newParagraph = document.createElement('p')
+    newParagraph.textContent = todo.title
+    document.querySelector('body').appendChild(newParagraph)
 })

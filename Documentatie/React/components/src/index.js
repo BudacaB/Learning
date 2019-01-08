0422b134c4ebace1438eb32f6091d4c43571d2c1
BudacaB <budaca.b@gmail.com>
2019-01-09 00:13:59 +0200

    react props, class intro

--- Documentatie/React/components/src/index.js
+++ Documentatie/React/components/src/index.js
@@ -2,15 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Sam" 
+                timeAgo="Today at 4:45PM"
+                postText="Hey there"
+                postImg={faker.image.avatar()} />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Alex" 
+                timeAgo="Today at 2:00AM"
+                postText="Hello"
+                postImg={faker.image.avatar()} />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Jane" 
+                timeAgo="Yesterday at 5:00PM"
+                postText="How are you"
+                postImg={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     );
 };

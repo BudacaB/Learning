a94709600a7c3b0eca1b2e17a1670abf9993c920
BudacaB <budaca.b@gmail.com>
2019-03-02 20:51:07 +0200

    adding elements via DOM, handling user interaction

--- Documentatie/JS course/notes-app/notes-app.js
+++ Documentatie/JS course/notes-app/notes-app.js
@@ -1,11 +1,14 @@
-/* const p = document.querySelector('p')
-p.remove() */
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habits to work on',
+    body: 'Exercise. Eating a bit better'
+}, {
+    title: 'Office modifications',
+    body: 'Get a new seat'
+}]
 
-const ps = document.querySelectorAll('p')
-
-ps.forEach(function(p) {
-    p.textContent = '*****'
-
-    //console.log(p.textContent)
-    //p.remove()
+document.querySelector('button').addEventListener('click', function(e) {
+    e.target.textContent = 'The button was clicked'
 })
--- Documentatie/JS course/todo-app/todo-app.js
+++ Documentatie/JS course/todo-app/todo-app.js
@@ -1,7 +1,45 @@
-const removeThe = document.querySelectorAll('p')
+const todos = [{
+    title: 'Workout',
+    completed: true
+    }, {
+    title: 'Brush teeth',
+    completed: true
+    }, {
+    title: 'Learn German',
+    completed: true
+    }, {
+    title: 'Learn to code',
+    completed: false
+    }, {
+    title: 'Do dishes',
+    completed: false
+}]
 
-removeThe.forEach(function(p) {
-    if (p.textContent.toLowerCase().includes('the')) {
-        p.remove()
+// You have 2 todos left (p element)
+// Add a p for each todo above (use text value)
+
+/* let todosLeft = 0;
+todos.forEach(function(todo) {
+    if (!todo.completed) {
+        todosLeft++;
     }
+}) */
+
+const incompleteTodos = todos.filter(function(todo) {
+    return !todo.completed
+})
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function(todo) {
+    const addTodo = document.createElement('p')
+    addTodo.textContent = todo.title
+    document.querySelector('body').appendChild(addTodo)
+})
+
+// Listen for new todo creation
+document.querySelector('button').addEventListener('click', function() {
+    console.log("I'm adding a new todo")
 })

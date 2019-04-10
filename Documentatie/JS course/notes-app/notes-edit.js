d2f7de08d4aafebaf33cd3fb172fad561d591147
BudacaB <budaca.b@gmail.com>
2019-04-11 01:05:59 +0300

    the edit note page p2 done

--- Documentatie/JS course/notes-app/notes-edit.js
+++ Documentatie/JS course/notes-app/notes-edit.js
@@ -1,3 +1,6 @@
+const titleElement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const removeElement = document.querySelector('#remove-note')
 const noteId = location.hash.substring(1)
 const notes = getSavedNotes()
 const note = notes.find(function(note) {
@@ -8,5 +11,20 @@ if (note === undefined) {
     location.assign('/index.html')
 }
 
-document.querySelector('#note-title').value = note.title
-document.querySelector('#note-body').value = note.body
+titleElement.addEventListener('input', function(e){
+    note.title = e.target.value
+    saveNotes(notes)
+})
+titleElement.value = note.title
+
+bodyElement.addEventListener('input', function(e){
+    note.body = e.target.value
+    saveNotes(notes)
+})
+bodyElement.value = note.body
+
+removeElement.addEventListener('click', function() {
+    removeNote(note.id)
+    saveNotes(notes)
+    location.assign('/index.html')
+})

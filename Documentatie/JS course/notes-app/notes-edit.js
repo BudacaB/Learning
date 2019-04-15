b02760869f569ba5afd65319a75504928822120f
BudacaB <budaca.b@gmail.com>
2019-04-16 00:37:58 +0300

    listening for local storage changes in the browser

--- Documentatie/JS course/notes-app/notes-edit.js
+++ Documentatie/JS course/notes-app/notes-edit.js
@@ -2,29 +2,46 @@ const titleElement = document.querySelector('#note-title')
 const bodyElement = document.querySelector('#note-body')
 const removeElement = document.querySelector('#remove-note')
 const noteId = location.hash.substring(1)
-const notes = getSavedNotes()
-const note = notes.find(function(note) {
-    return note.id ===noteId
+let notes = getSavedNotes()
+let note = notes.find(function(note) {
+    return note.id === noteId
 })
 
 if (note === undefined) {
     location.assign('/index.html')
 }
 
+titleElement.value = note.title
+bodyElement.value = note.body
+
 titleElement.addEventListener('input', function(e){
     note.title = e.target.value
     saveNotes(notes)
 })
-titleElement.value = note.title
 
 bodyElement.addEventListener('input', function(e){
     note.body = e.target.value
     saveNotes(notes)
 })
-bodyElement.value = note.body
 
 removeElement.addEventListener('click', function() {
     removeNote(note.id)
     saveNotes(notes)
     location.assign('/index.html')
+})
+
+window.addEventListener('storage', function(e) {
+    if (e.key === 'notes' ) {
+        notes = JSON.parse(e.newValue)
+        let note = notes.find(function(note) {
+            return note.id === noteId
+        })
+        
+        if (note === undefined) {
+            location.assign('/index.html')
+        }
+        
+        titleElement.value = note.title
+        bodyElement.value = note.body
+    }
 })

35d40557544c4e1896f7d2fa841d494970dd72e7
BudacaB <budaca.b@gmail.com>
2019-05-02 01:35:56 +0300

    arrow functions II

--- Documentatie/JS course/notes-app/notes-functions.js
+++ Documentatie/JS course/notes-app/notes-functions.js
@@ -1,5 +1,5 @@
 // Read existing notes from localStorage
-const getSavedNotes = function() {
+const getSavedNotes = () => {
     const notesJSON = localStorage.getItem('notes')
 
     if (notesJSON !== null) {
@@ -10,22 +10,20 @@ const getSavedNotes = function() {
 }
 
 // Save the notes to localStorage
-const saveNotes = function(notes){
+const saveNotes = (notes) => {
     localStorage.setItem('notes', JSON.stringify(notes))
 }
 
 // Remove a note from the list
-const removeNote = function(id) {
-    const noteIndex = notes.findIndex(function (note) {
-        return note.id === id
-    })
+const removeNote = (id) => {
+    const noteIndex = notes.findIndex((note) => note.id === id)
     if(noteIndex > -1) {
         notes.splice(noteIndex, 1)
     }
 }
 
 // Generate the DOM structure for a note
-const generateNoteDOM = function(note) {
+const generateNoteDOM = (note) => {
     const noteEl = document.createElement('div')
     const textEl = document.createElement('a')
     const button = document.createElement('button')
@@ -33,7 +31,7 @@ const generateNoteDOM = function(note) {
     //Setup the remove note button
     button.textContent = 'x'
     noteEl.appendChild(button)
-    button.addEventListener('click', function() {
+    button.addEventListener('click', () => {
         removeNote(note.id)
         saveNotes(notes)
         renderNotes(notes, filters)
@@ -53,7 +51,7 @@ const generateNoteDOM = function(note) {
 }
 
 // Sort your notes by one of three notes
-const sortNotes = function(notes, sortBy) {
+const sortNotes = (notes, sortBy) => {
     if (sortBy === 'byEdited') {
         return notes.sort(function(a, b) {
             if (a.updatedAt > b.updatedAt) {
@@ -90,21 +88,17 @@ const sortNotes = function(notes, sortBy) {
 }
 
 // Render application notes
-const renderNotes = function(notes, filters) {
+const renderNotes = (notes, filters) => {
     notes = sortNotes(notes, filters.sortBy)
-    const filteredNotes = notes.filter(function(note) {
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
+    const filteredNotes = notes.filter((note) => note.title.toLowerCase().includes(filters.searchText.toLowerCase()))
 
     document.querySelector('#notes').innerHTML = ''
     
-    filteredNotes.forEach(function(note) { 
+    filteredNotes.forEach((note) => { 
         const noteEl = generateNoteDOM(note)   
         document.querySelector('#notes').appendChild(noteEl)
     })
 }
 
 // Generate the last edited message
-const generateLastEdited = function(timestamp) {
-    return `Last edited ${moment(timestamp).fromNow()}`
-} 
+const generateLastEdited = (timestamp) => `Last edited ${moment(timestamp).fromNow()}` 
--- Documentatie/JS course/todo-app/todos-functions.js
+++ Documentatie/JS course/todo-app/todos-functions.js
@@ -1,4 +1,4 @@
-const getSavedTodos = function() {
+const getSavedTodos = () => {
     const todosJSON = localStorage.getItem('todos')
     if (todosJSON !== null) {
         return JSON.parse(todosJSON)
@@ -7,56 +7,50 @@ const getSavedTodos = function() {
     }
 }
 
-const saveTodos = function(todos) {
+const saveTodos = (todos) => {
     localStorage.setItem('todos', JSON.stringify(todos))
 }
 
-const removeTodo = function(id) {
-    const todoIndex = todos.findIndex(function(todo) {
-        return todo.id === id
-    })
+const removeTodo = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id)
     if(todoIndex > -1) {
         todos.splice(todoIndex, 1)
     }
 }
 
-const toggleTodo = function(id) {
-    const todo = todos.find(function(todo) {
-        return todo.id === id;
-    })
+const toggleTodo = (id) => {
+    const todo = todos.find((todo) => todo.id === id)
 
     if(todo !== undefined) {
         todo.completed = !todo.completed
     }
 }
 
-const renderTodos = function(todos, filters) {
-    const filteredTodos = todos.filter(function(todo) {
+const renderTodos = (todos, filters) => {
+    const filteredTodos = todos.filter((todo) => {
         const searchMatch = todo.title.toLowerCase().includes(filters.searchText.toLowerCase());
         const hideCompleted = !filters.checked || !todo.completed
         return searchMatch && hideCompleted
     })
 
-    const incompleteTodos = filteredTodos.filter(function(todo) {
-        return !todo.completed
-    })
+    const incompleteTodos = filteredTodos.filter((todo) => !todo.completed)
 
     document.querySelector('#todos').innerHTML = ''
     document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos))
 
-    filteredTodos.forEach(function(todo) {
+    filteredTodos.forEach((todo) => {
         document.querySelector('#todos').appendChild(generateTodoDOM(todo))
     })
 }
 
-const generateTodoDOM = function(todo) {
+const generateTodoDOM = (todo) => {
     const todoEl = document.createElement('div')
 
     const checkbox = document.createElement('input')
     checkbox.setAttribute('type', 'checkbox')
     checkbox.checked = todo.completed
     todoEl.appendChild(checkbox)
-    checkbox.addEventListener('change', function(){
+    checkbox.addEventListener('change', () => {
         toggleTodo(todo.id)
         saveTodos(todos)
         renderTodos(todos, filters)
@@ -65,7 +59,7 @@ const generateTodoDOM = function(todo) {
     const textEl = document.createElement('span')
     const button = document.createElement('button')
     button.textContent = 'x'
-    button.addEventListener('click', function() {
+    button.addEventListener('click', () => {
         removeTodo(todo.id)
         saveTodos(todos)
         renderTodos(todos, filters)
@@ -86,7 +80,7 @@ const generateTodoDOM = function(todo) {
         
 }
 
-const generateSummaryDOM = function(incompleteTodos) {
+const generateSummaryDOM = (incompleteTodos) => {
     
     const summary = document.createElement('h2')
     summary.textContent = `You have ${incompleteTodos.length} todos left`

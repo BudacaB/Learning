e2d80ada60e9e407a0117ea8f513175baa1d8941
BudacaB <budaca.b@gmail.com>
2019-05-08 00:33:08 +0300

    catching and throwing errors

--- Documentatie/JS course/functions/grade-calc.js
+++ Documentatie/JS course/functions/grade-calc.js
@@ -3,27 +3,37 @@
 // A 90-100, B 80-89, C 70-79, D 60-69, F 0-59
 
 const gradeCalc = function(studentScore, possibleScore) {
+    if (typeof studentScore !== 'number' || typeof possibleScore !== 'number') {
+        throw Error('Please input numbers')
+    }
+
     const percent = studentScore / possibleScore * 100
-    let letterGrade = ''
+        let letterGrade = ''
+    
+        if (90 <= percent) {
+            letterGrade = 'A'
+        }
+        else if (80 <= percent) {
+            letterGrade = 'B'
+        }
+        else if (70 <= percent) {
+            letterGrade = 'C'
+        }
+        else if (60 <= percent) {
+            letterGrade = 'D'
+        }
+        else {
+            letterGrade = 'F'
+        }
+    
+        return `You got a ${letterGrade} (${percent}%)`
 
-    if (90 <= percent) {
-        letterGrade = 'A'
-    }
-    else if (80 <= percent) {
-        letterGrade = 'B'
-    }
-    else if (70 <= percent) {
-        letterGrade = 'C'
-    }
-    else if (60 <= percent) {
-        letterGrade = 'D'
-    }
-    else {
-        letterGrade = 'F'
-    }
-
-    return `You got a ${letterGrade} (${percent}%)`
 }
 
-const result = gradeCalc(87, 100)
-console.log(result)
+try {
+    const result = gradeCalc('check', 100)
+    console.log(result)
+} catch (e) {
+    console.log(e.message)
+}
+

faa4ebec31a26d6cde4dd31c6a97f4ebb19d5f6e
BudacaB <budaca.b@gmail.com>
2019-05-18 14:57:40 +0300

    hangman challenge I

--- Documentatie/JS course/oop/hangman.js
+++ Documentatie/JS course/oop/hangman.js
@@ -1,10 +1,25 @@
-const Hangman = function (word, guesses) {
-    this.word = word
-    this.guesses = guesses
+const Hangman = function (word, remainingGuesses) {
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['c', 'e']
 }
 
-const game = new Hangman ('park', 3)
-console.log(game)
+Hangman.prototype.getPuzzle = function() {
+    let puzzle = []
 
-const secondGame = new Hangman ('mountain', 5)
-console.log(secondGame)
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    })
+
+    return puzzle
+}
+
+const game = new Hangman ('Cat', 2)
+console.log(game.getPuzzle())
+
+const secondGame = new Hangman ('New Jersey', 4)
+console.log(secondGame.getPuzzle())

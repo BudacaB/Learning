85e2ed878ae361f00aaf56bfb47c78572984deb7
BudacaB <budaca.b@gmail.com>
2019-07-26 01:07:26 +0300

    fetch api

--- Documentatie/JS course/hangman/app.js
+++ Documentatie/JS course/hangman/app.js
@@ -16,17 +16,30 @@ window.addEventListener('keypress', (e) => {
     statusEl.textContent = game1.statusMessage;
 })
 
+
 getPuzzle('2').then((puzzle) => {
     console.log(puzzle)
-}, (err) => {
+}).catch((err) => {
     console.log(`Error: ${err}`)
 })
 
 
-getCountry('RO').then((country) => {
-    console.log(country.name)
-}, (err) => {
-    console.log(`Error: ${err}`)
-})
+// getCountry('RO').then((country) => {
+//     console.log(country.name)
+// }, (err) => {
+//     console.log(`Error: ${err}`)
+// })
 
+
+// fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
+//     if (response.status === 200) {
+//         return response.json()
+//     } else {
+//         throw new Error('Unable to fetch the puzzle')
+//     }
+// }).then((data) => {
+//     console.log(data.puzzle)
+// }).catch((error) => {
+//     console.log(error)
+// })
 

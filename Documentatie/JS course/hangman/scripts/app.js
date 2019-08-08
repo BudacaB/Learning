a96c5e80e43f8973ec367ed86e2dbc9c342c413c
BudacaB <budaca.b@gmail.com>
2019-08-09 02:54:35 +0300

    hangman app theme

--- Documentatie/JS course/hangman/scripts/app.js
+++ Documentatie/JS course/hangman/scripts/app.js
@@ -13,8 +13,20 @@ window.addEventListener('keypress', (e) => {
 })
 
 const render = () => {
-    puzzleEl.textContent = game1.puzzle;
+    puzzleEl.innerHTML = '';
     statusEl.textContent = game1.statusMessage;
+
+    //own solution
+    game1.puzzle.split('').forEach((letter) =>{
+        puzzleEl.innerHTML = puzzleEl.innerHTML.concat('<span>' + letter + '</span>')
+    })
+
+    //course solution
+    // game1.puzzle.split('').forEach((letter) =>{
+    //     const letterEl = document.createElement('span')
+    //     letterEl.textContent = letter
+    //     puzzleEl.appendChild(letterEl)
+    // })
 }
 
 const startGame = async () => {

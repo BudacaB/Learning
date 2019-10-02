828fc405ed6fe445ae304c85e92cd8006e9f2c9a
BudacaB <budaca.b@gmail.com>
2019-10-03 01:58:15 +0300

    the spread syntax

--- Documentatie/JS course/boilerplate/src/index.js
+++ Documentatie/JS course/boilerplate/src/index.js
@@ -1,19 +1,18 @@
-// the Rest parameter
-
-const calculateAverage = (thing, ...numbers) => {
-  //return (num1 + num2) / 2;
-  let sum = 0;
-  numbers.forEach(num => (sum += num));
-  const average = sum / numbers.length;
-  return `The average ${thing} is ${average}`;
-};
-
-console.log(calculateAverage("age", 0, 100, 88, 64));
-
-function printTeam(team, coach, ...players) {
+const printTeam = (team, coach, ...players) => {
   console.log(`Team: ${team}`);
   console.log(`Coach: ${coach}`);
   console.log(`Players: ${players.join(", ")}`);
-}
+};
 
-printTeam("Liberty", "Casey Penn", "Marge", "Aiden", "Herbert");
+const team = {
+  name: "Liberty",
+  coach: "Casey Penn",
+  players: ["Marge", "Aiden", "Herbert"]
+};
+
+printTeam(team.name, team.coach, ...team.players);
+
+let cities = ["Barcelona", "Cape Town", "Bordeaux"];
+cities = [...cities, "Santiago"];
+
+console.log(cities);

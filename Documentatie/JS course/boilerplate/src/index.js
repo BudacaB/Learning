e785b01f0081d6be3f09f0dde9365a1950248e92
BudacaB <budaca.b@gmail.com>
2019-10-04 00:48:43 +0300

    object spread syntax

--- Documentatie/JS course/boilerplate/src/index.js
+++ Documentatie/JS course/boilerplate/src/index.js
@@ -16,3 +16,34 @@ let cities = ["Barcelona", "Cape Town", "Bordeaux"];
 cities = [...cities, "Santiago"];
 
 console.log(cities);
+
+let house = {
+  bedrooms: 2,
+  bathrooms: 1.5,
+  year: 2017
+};
+let newHouse = {
+  basement: true,
+  bedrooms: 3,
+  ...house
+};
+
+console.log(house);
+console.log(newHouse);
+
+const person = {
+  name: "Bog",
+  age: 32
+};
+
+const location = {
+  city: "Buch",
+  country: "RO"
+};
+
+const overview = {
+  ...person,
+  ...location
+};
+
+console.log(overview);

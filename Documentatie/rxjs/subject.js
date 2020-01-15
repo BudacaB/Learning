e2b4c4ab6ef2b3b2bd6293758fede41394808a94
BudacaB <budaca.b@gmail.com>
2020-01-16 01:27:31 +0200

    rxjs subject

---
+++ Documentatie/rxjs/subject.js
@@ -0,0 +1,25 @@
+const rxjs = require("rxjs");
+
+var subject = new rxjs.Subject();
+
+subject.subscribe({
+  next: function(value) {
+    console.log(value);
+  },
+  error: function(error) {
+    console.log(error);
+  },
+  complete: function() {
+    console.log("Complete");
+  }
+});
+
+subject.subscribe({
+  next: function(value) {
+    console.log(value);
+  }
+});
+
+subject.next("A new data piece");
+subject.complete();
+subject.next("A new data piece again");

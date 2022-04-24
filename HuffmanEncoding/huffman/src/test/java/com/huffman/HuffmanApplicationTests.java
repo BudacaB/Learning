6c24e7692f9fa5c88025585d35c90ff50d37e663
BudacaB <budaca.b@gmail.com>
2022-04-24 12:49:41 +0300

    huffman

---
+++ HuffmanEncoding/huffman/src/test/java/com/huffman/HuffmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huffman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuffmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

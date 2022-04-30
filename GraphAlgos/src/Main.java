e1e54559ddbe96bc7e00de1d390aad1093b026ad
BudacaB <budaca.b@gmail.com>
2022-04-30 21:33:37 +0300

    bfs and dfs

--- GraphAlgos/src/Main.java
+++ GraphAlgos/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     // The graph
     static Map<String, ArrayList<String>> adjacencyList = new HashMap<>();
+    static int steps = 0;
 
     public static void main(String[] args) {
         String[] airports = {"PHX", "BKK", "OKC", "JFK", "LAX", "MEX", "EZE", "HEL", "LOS", "LAP", "LIM"};
@@ -32,6 +33,8 @@ public class Main {
 
         System.out.println(adjacencyList);
 
+//        bfs("PHX");
+        dfs("PHX", null);
     }
 
     // Add node
@@ -44,4 +47,52 @@ public class Main {
         adjacencyList.get(origin).add(destination);
         adjacencyList.get(destination).add(origin);
     }
+
+    // BFS breadth first search
+    public static void bfs(String start) {
+
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>(List.of(start));
+
+        while (queue.size() > 0) {
+            String airport = queue.poll();
+            ArrayList<String> destinations = adjacencyList.get(airport);
+
+            for (String destination : destinations) {
+
+                if (destination == "BKK") {
+                    System.out.println("found it");
+                }
+
+                if (!visited.contains(destination)) {
+                    visited.add(destination);
+                    queue.add(destination);
+                    System.out.println(destination);
+                }
+            }
+        }
+    }
+
+    // DFS depth first search
+    public static void dfs(String start, Set<String> v) {
+        System.out.println(start);
+        steps++;
+        Set<String> visited = Objects.requireNonNullElseGet(v, HashSet::new);
+        visited.add(start);
+
+        ArrayList<String> destinations = adjacencyList.get(start);
+
+        for (String destination : destinations) {
+            if (destination == "BKK") {
+                System.out.println("DFS found Bangkok in " + steps + " steps");
+                return;
+            }
+
+            if (!visited.contains(destination)) {
+                dfs(destination, visited);
+            }
+        }
+    }
 }
+
+// Big O is O(V + E) -> Nodes + Edges -> O(n)
